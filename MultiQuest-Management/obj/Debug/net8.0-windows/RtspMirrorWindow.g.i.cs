15aaa8a91a06a6ecbfb70e22be3729ed81c0fbfa
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2026-06-26 21:35:50 +0900

    [Fix] ADB 정리 작업

--- MultiQuest-Management/obj/Debug/net8.0-windows/RtspMirrorWindow.g.i.cs
+++ MultiQuest-Management/obj/Debug/net8.0-windows/RtspMirrorWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace MultiQuest_Management {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MultiQuest-Management;V1.0.0.0;component/rtspmirrorwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MultiQuest-Management;component/rtspmirrorwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\RtspMirrorWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
